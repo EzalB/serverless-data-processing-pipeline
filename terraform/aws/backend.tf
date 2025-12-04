69e62ba64d76d58ad1528d22bddefbd580915a90
Rusheel Dalal <rusheel1309@gmail.com>
2025-12-04 16:22:52 +0530

    TF backend fixes

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-serverless-pipeline"
+    bucket         = "serverless-pipeline-tf-state"
     key            = "aws/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"

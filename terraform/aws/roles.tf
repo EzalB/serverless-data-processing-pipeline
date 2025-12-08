8df2b1aae1fde5de6bcbd012366c8c35949ce653
Rusheel Dalal <rusheel1309@gmail.com>
2025-12-08 18:39:00 +0530

    AWS Terraform UploaderPolicy update

--- terraform/aws/roles.tf
+++ terraform/aws/roles.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 # ------------------------
 # Lambda IAM Role
 # ------------------------
@@ -49,6 +51,25 @@ resource "aws_iam_policy" "uploader_policy" {
         Resource = [
           aws_s3_bucket.data_bucket.arn,
           "${aws_s3_bucket.data_bucket.arn}/*"
+        ]
+      },
+      {
+        "Sid": "AllowTerraformReadIAMResources",
+        "Effect": "Allow",
+        "Action": [
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListPolicyVersions",
+          "iam:ListPolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListPolicyTags"
+        ],
+        "Resource": [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/*",
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*"
         ]
       }
     ]
